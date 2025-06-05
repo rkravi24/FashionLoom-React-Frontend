14e16c3f50e26ecc503f03248b3c17ee5d9bba89
Ravi kumar <mrkravi03@gmail.com>
2025-06-05 23:57:39 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return(
+        <>
+            <div className="flex items-center justify-center w-full bg-gray-700 p-2 text-white">
+                <p className="text-gray-400 font-bold">&lt;</p>
+                <p className="font-bold underline ml-10 mr-10">Maximum 15% off on premimum Collection</p>
+                <p className="text-gray-400 font-bold">&gt;</p>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
