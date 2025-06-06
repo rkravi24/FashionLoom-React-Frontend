9f8ee26c89bacc918c8abb8c76a969ea7bf8385b
Ravi kumar <mrkravi03@gmail.com>
2025-06-06 23:49:19 +0530

    add hero section

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,27 @@
+const HeroSection = () => {
+    return (
+        <div className="flex items-center w-full h-100 rounded-lg overflow-hidden">
+            <div className="h-full w-full  bg-cover bg-center bg-no-repeat  flex items-center justify-center" style={{
+                backgroundImage: "url('https://images.pexels.com/photos/789303/pexels-photo-789303.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+            }}>
+            </div>
+            <div className="h-full w-full  bg-cover bg-center bg-no-repeat  flex items-center justify-center" style={{
+                backgroundImage: "url('https://images.pexels.com/photos/1162983/pexels-photo-1162983.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+            }}>
+            </div>
+            <div className="h-full w-full  bg-cover bg-center bg-no-repeat  flex items-center justify-center" style={{
+                backgroundImage: "url('https://images.pexels.com/photos/28994695/pexels-photo-28994695/free-photo-of-trendy-woman-posing-in-retro-style-outfit-indoors.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')"
+            }}>
+            </div>
+
+            <div className="absolute flex-col ml-[150px]">
+                <h1 className=" text-5xl font-semibold">Style that Moves with You</h1>
+                <p className="mt-2 text-xl">Premium Quality. Timeless Design. Effortless Comfort.</p>
+                <button className="bg-white mt-4 text-black p-4 w-[200px] rounded-xl ">Explore Now</button>
+                <p className="mt-2">Join 100,000+ trendsetters who made the switch.</p>
+            </div>
+        </div>
+    );
+};
+
+export default HeroSection
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,25 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
 const Navbar = () => {
-    return(
+    return (
         <>
-            <div className="flex items-center justify-center w-full bg-gray-700 p-2 text-white">
+            <div className="flex items-center justify-center w-full bg-gray-700 p-3 text-white">
                 <p className="text-gray-400 font-bold">&lt;</p>
                 <p className="font-bold underline ml-10 mr-10">Maximum 15% off on premimum Collection</p>
                 <p className="text-gray-400 font-bold">&gt;</p>
+            </div>
+            <div className="flex items-center justify-evenly bg-amber-100 p-4">
+                <h3 className="font-semibold text-black">Shop</h3>
+                <h3 className="font-semibold text-black">Offer-Zone</h3>
+                <h1 className="font-bold text-3xl text-black">FASHION LOOM</h1>
+                <div className="flex items-center justify-evenly w-70">
+                    <h3 className="font-semibold text-black mr-4">Review</h3>
+                    <h3 className="font-semibold text-black mr-4">About</h3>
+                    <FontAwesomeIcon icon={faUser} className='text-gray-600 text-xl mr-5'/>
+                    <FontAwesomeIcon icon={faCartShopping} className="text-gray-600 text-xl" />
+                </div>
             </div>
         </>
     )
