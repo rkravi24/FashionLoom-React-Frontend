d790a87e05a998f25336648f91ecc3e1c1bee917
Ravi kumar <mrkravi03@gmail.com>
2025-06-15 01:24:54 +0530

    review section updated

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -13,24 +13,57 @@ const Review = () => {
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
         },
         {
+            name: "Zoya",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Aarohi",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Zoya",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Aarohi",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Zoya",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Aarohi",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Zoya",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
+            name: "Aarohi",
+            text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
+        },
+        {
             name: "Aanya",
             text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, eum?"
         },
     ]
-    return(
-        <>
-            <div className="flex flex-col flex-wrap items-center p-10 justify-center bg-amber-100">
-                <h1 className="text-3xl font-mono "> :) Hear from Happy Shoppers</h1>
-               <div className="flex">
-                 {review.map((item) =>(
-                    <div className="card h-full p-4 mt-4 mx-4 w-70 text-justify bg-red-100 rounded border-r-2 border-b-2 border-l-1 border-t-1">
-                        <p>~{item.name}</p>
-                        <p>{item.text}</p>
-                    </div>
-                ))}
-               </div>
+    return (
+        <div className="flex flex-col flex-wrap items-center p-10 justify-center bg-amber-100">
+            <h1 className="text-3xl font-mono">:) Hear from Happy Shoppers</h1>
+            <div className="marquee-track">
+                <div className="marquee-content flex">
+                    {review.map((item, index) => (
+                        <div key={index} className="p-4 mt-4 mx-5 w-[280px] border flex flex-col bg-red-100 rounded break-words whitespace-normal">
+                            <p className="font-semibold text-lg">~ {item.name}</p>
+                            <p className="text-sm mt-2">{item.text}</p>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </>
+        </div>
+
     )
 }
 
